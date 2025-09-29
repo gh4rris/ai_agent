1caaf5421cdb3a75210e81d40edbba64fed1cf79
gh4rris <gharris90@hotmail.co.uk>
2025-09-29 18:59:39 +0100

    feedback loop

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google import genai
 from google.genai import types
 from prompts import system_prompt
 from call_function import call_function, available_functions
+from config import MAX_ITERS
 
 def main():
     load_dotenv()
@@ -20,11 +21,24 @@ def main():
     api_key = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    content = " ".join(args)
-    messages = [types.Content(role="user", parts=[types.Part(text=content)])]
+    user_prompt = " ".join(args)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
     
-    
-    generate_content(client, messages, verbose)
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
 def generate_content(client, messages, verbose):
@@ -37,21 +51,31 @@ def generate_content(client, messages, verbose):
         )
 
     if verbose:
-        print(f"User prompt: {messages[-1].parts[0].text}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
 
-    if not response.function_calls:
-        print(response.text)
-        return
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
     for function_call_part in response.function_calls:
         result = call_function(function_call_part, verbose)
         if not result.parts[0].function_response.response:
             raise Exception("no response from call function")
         if verbose:
             print(f"-> {result.parts[0].function_response.response}")
+        function_responses.append(result.parts[0])
+    
+    if not function_responses:
+        raise Exception("no function responses generated.") 
+    
+    messages.append(types.Content(role="user", parts=function_responses))
+            
 
 
 if __name__ == "__main__":
